050acd734343d36b9c8ff10a53f1497e840a3b8e
rossning92 <rossning92@github.com>
2020-01-25 12:59:42 -0800

    Add three.js support.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import './myfile.css';
-//const WebMWriter = require('webm-writer');
-//import CCapture from 'ccapture.js';
+// const WebMWriter = require('webm-writer');
+// import CCapture from 'ccapture.js';
 
 import * as dat from 'dat.gui';
-import * as PIXI from 'pixi.js'
 import TWEEN from '@tweenjs/tween.js';
-import { MotionBlurFilter } from '@pixi/filter-motion-blur';
-
-console.log("hello, world");
 
 
-let renderer, stage;
-let velocity = { max_vx: 3, min_vx: 1, max_vy: 3, min_vy: 1 };
+import * as THREE from 'three';
+
+
+
+
 let capturer = null;
+let renderer;
+let scene;
+let camera;
+let sphere;
+
 
 let options = {
   /* Recording options */
@@ -30,54 +34,7 @@ gui.add(options, 'stop');
 
 
 
-function createCircles() {
 
-
-
-  let rectRotation = new PIXI.Graphics();
-  rectRotation.x = renderer.width / 2;
-  rectRotation.y = renderer.height / 2;
-  rectRotation.rotation = -45 * Math.PI / 180;
-  stage.addChild(rectRotation);
-
-  // new TWEEN.Tween(rectRotation)
-  //     .to({rotation: 45 * Math.PI / 180}, 500)
-  //     .delay(200)
-  //     .start();
-
-
-
-  // CREATE SOME RECTANGLES FOR THE WIPE
-  var DURATION = 5000
-  var INTERVAL = 5
-  var SINGLE_RECT_DURATION = 500
-  let setPointColors = ["0xf39909", "0xa51981", "0xff1981", "0xf30009"];
-  for (var i = 0; i < Math.floor(DURATION / INTERVAL); i++) {
-    let rect = new PIXI.Graphics();
-    let colorIndex = Math.floor(Math.random() * setPointColors.length);
-    rect.beginFill(setPointColors[colorIndex], 1);
-
-    rect.drawRect(0, 0, renderer.width, renderer.height * 2);
-    rect.pivot.x = renderer.width / 2;
-    rect.pivot.y = renderer.height;
-    // rect.rotation = -45 * 3.14 / 180;
-
-    rectRotation.addChild(rect);
-
-
-    rect.width = 0;
-
-    new TWEEN.Tween(rect)
-      .to({
-        x: i % 2 == 0 ? renderer.width * 1.5 : -renderer.width * 1.5,
-        width: renderer.width
-      }, SINGLE_RECT_DURATION)
-      .delay(200 + i * INTERVAL)
-      .easing(TWEEN.Easing.Exponential.InOut)
-      .start();
-
-  }
-}
 function initRecording() {
   capturer = new CCapture({
     verbose: true,
@@ -103,75 +60,69 @@ function stopRecording() {
   }
 }
 
-function resetAnimation() {
-  // Custom PIXI application
-  // backgroundColor: "0xffffff"
-  renderer = new PIXI.Renderer({ width: 600, height: 600, antialias: true, transparent: false });
-  renderer.autoResize = false;
-  // renderer.resize(window.innerWidth, window.innerHeight);
 
-  /* Add the canvas to the HTML document */
-  let c = document.getElementsByTagName("canvas")[0];
-  if (c)
-    document.body.removeChild(c);
-  document.body.appendChild(renderer.view);
-
-  /* Create a container object called the `stage` */
-  stage = new PIXI.Container();
-  // stage.filters = [new MotionBlurFilter([10, 20], 9)];
+function render() {
+  sphere.rotation.x += 0.005;
+  sphere.rotation.y += 0.005;
+  renderer.render(scene, camera);
 }
-resetAnimation();
-createCircles();
 
 
-const g = new PIXI.Graphics();
-g.lineStyle(4, 0x7F7F7F, 1);
-// g.beginFill(0xAA4F08);
-g.drawRect(10, 10, 100, 100);
-// g.endFill();
-g.pivot.x = 55;
-g.pivot.y = 55;
-g.x = 100;
-g.y = 100;
-// g.angle = 45;
+function resize(width, height) {
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+}
 
-stage.addChild(g);
+function setupScene(width, height) {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement);
 
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 500);
+  camera.position.set(0, 0, 8);
 
-// Text
-const style = new PIXI.TextStyle({
-  fontFamily: "Roboto",
-  fontSize: 80,
-  align: 'center',
-  fill: 0xffffff
-});
-const text = new PIXI.Text('Test', style);
-text.x = 0;
-text.y = 300;
-text.anchor.set(0.5)
-text.alpha = 0;
-stage.addChild(text);
+  const light0 = new THREE.PointLight(0xffffff, 1, 0);
+  light0.position.set(0, 200, 0);
+  scene.add(light0);
 
-new TWEEN.Tween(text)
-  .to({
-    x: 300,
-    alpha: 1
-  }, 500)
-  .easing(TWEEN.Easing.Exponential.InOut)
-  .start();
+  const light1 = new THREE.PointLight(0xffffff, 1, 0);
+  light1.position.set(100, 200, 100);
+  scene.add(light1);
 
+  const light2 = new THREE.PointLight(0xffffff, 1, 0);
+  light2.position.set(-100, -200, -100);
+  scene.add(light2);
 
-function update(time) {
+  scene.add(new THREE.AmbientLight(0x000000));
+
+  sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 16, 16),
+    new THREE.MeshPhongMaterial({
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      shading: THREE.FlatShading
+    })
+  );
+  scene.add(sphere);
+}
+
+function animate(time) {
   TWEEN.update(time);
 
   /* Loop this function */
-  requestAnimationFrame(update);
+  requestAnimationFrame(animate);
 
-  /* Tell the `renderer` to `render` the `stage` */
-  renderer.render(stage);
+  render();
 
   /* Record Video */
-  if (capturer) capturer.capture(renderer.view);
+  if (capturer) capturer.capture(renderer.domElement);
 }
-requestAnimationFrame(update);
 
+setupScene(800, 800);
+resize(800, 800);
+requestAnimationFrame(animate);
